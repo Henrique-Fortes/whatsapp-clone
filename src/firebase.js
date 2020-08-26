88a22aedc2b838f0be2875cd19fca9f14dc4cebf
Henrique H. da Silva Fortes <Henrique Fortes>
2020-08-26 09:27:44 -0300

    Commit whatsapp-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbqqNlNTn_d09XgaQBiVNl-7RT9kpeFmI",
+    authDomain: "whatsapp-clone-38bc6.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-38bc6.firebaseio.com",
+    projectId: "whatsapp-clone-38bc6",
+    storageBucket: "whatsapp-clone-38bc6.appspot.com",
+    messagingSenderId: "522894880723",
+    appId: "1:522894880723:web:af18211ef82aab2eae8926",
+    measurementId: "G-CFLQTMDCK3"
+  };
